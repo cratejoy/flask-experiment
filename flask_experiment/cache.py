ad6bd8154dec899c8bb3f61bee26f6e13758e273
Alex Morse <alex@lark.io>
2013-04-17 18:37:33 -0500

    Disabling logspam

--- flask_experiment/cache.py
+++ flask_experiment/cache.py
@@ -12,7 +12,7 @@ class ExperimentTemplateCache(LRUCache):
 
     def __contains__(self, key):
         """Check if a key exists in this cache."""
-        print "Calling parent for contains", self.experiment_key(key)
+        #print "Calling parent for contains", self.experiment_key(key)
         return LRUCache.contains(self, self.experiment_key(key))
 
     def __getitem__(self, key):
@@ -21,18 +21,16 @@ class ExperimentTemplateCache(LRUCache):
 
         Raise an `KeyError` if it does not exist.
         """
-        print "Calling parent for __get__", self.experiment_key(key)
-        print self._mapping
+        #print "Calling parent for __get__", self.experiment_key(key)
+        #print self._mapping
         return LRUCache.__getitem__(self, self.experiment_key(key))
-
 
     def __setitem__(self, key, value):
         """Sets the value for an item. Moves the item up so that it
         has the highest priority then.
         """
-        print "Calling parent for __set__", self.experiment_key(key)
+        #print "Calling parent for __set__", self.experiment_key(key)
         return LRUCache.__setitem__(self, self.experiment_key(key), value)
-
 
     def __delitem__(self, key):
         """Remove an item from the cache dict.
